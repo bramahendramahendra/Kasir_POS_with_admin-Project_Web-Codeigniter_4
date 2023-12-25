a156ceb2396cc56b1cd90415186117d8d5e2f5e5
bramahendramahendra <bramahendramahendra@gmail.com>
2023-12-25 17:21:18 +0700

    MVC read create menu satuan

--- app/Controllers/Satuan.php
+++ app/Controllers/Satuan.php
@@ -3,9 +3,16 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ModelSatuan;
 
 class Satuan extends BaseController
 {
+
+    public function __construct() 
+    {
+        $this->ModelSatuan = new ModelSatuan();
+    }
+
     public function index()
     {
         $data = [
@@ -13,8 +20,17 @@ class Satuan extends BaseController
             'subjudul' => 'Satuan',
             'menu' => 'masterdata',
             'submenu' => 'satuan' ,
-            'page' => 'v_satuan'
+            'page' => 'v_satuan',
+            'satuan' => $this->ModelSatuan->AllData(),
         ];
         return view('v_template', $data);
+    }
+
+    public function InsertData()
+    {
+        $data = ['nama_satuan' => $this->request->getPost('nama_satuan')];
+        $this->ModelSatuan->InsertData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil DItambahkan !!');
+        return redirect()->to('Satuan');
     }
 }
---
+++ app/Models/ModelSatuan.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelSatuan extends Model
+{
+    protected $table            = 'tbl_satuan';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function AllData() {
+        return $this->db->table('tbl_satuan')
+            ->get()
+            ->getResultArray();
+    }
+
+    public function InsertData($data) 
+    {
+        $this->db->table('tbl_satuan')->insert($data);    
+    }
+}
